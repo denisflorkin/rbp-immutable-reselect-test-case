e2801116a06d6913a33268e42440b431a208443f
denisflorkin <denisflorkin@gmail.com>
2017-02-03 15:34:19 +0100

    improved/add test to poriflepagesettings

--- app/containers/ProfilePage/sagas.js
+++ app/containers/ProfilePage/sagas.js
@@ -1,10 +1,11 @@
 /**
  * Gets the default data of the user
  */
-import { takeLatest } from 'redux-saga';
-import { call, put, fork } from 'redux-saga/effects';
+// import { takeLatest } from 'redux-saga';
+import { takeLatest, take, call, put, cancel } from 'redux-saga/effects';
 import request from 'utils/request';
 
+import { LOCATION_CHANGE } from 'react-router-redux';
 import { LOAD_PROFILE } from './constants';
 
 import {
@@ -14,7 +15,7 @@ import {
 } from './actions';
 
 
-export function* getUserData(action) {
+export function* getUserProfileData(action) {
   const requestURL =
     `https://localhost/projects/neoapi/user/${action.slug}/GET.json`
 
@@ -31,26 +32,26 @@ export function* getUserData(action) {
  * Watches for LOAD_DEFAULT_DATA actions and calls getDefaultData when one comes in.
  * By using `takeLatest` only the result of the latest API call is applied.
  */
-export function* getUserDataWatcher() {
-  yield fork(takeLatest, LOAD_PROFILE, getUserData);
-}
+// export function* getUserProfileDataWatcher() {
+//   yield fork(takeLatest, LOAD_PROFILE, getUserProfileData);
+// }
 
 /**
  * Root saga manages watcher lifecycle
  */
-export function* userData() {
+export function* userProfileData() {
   /* Should this be canceled on LOCATION_CHANGE ?? */
 
   // Fork watcher so we can continue execution
-  // const watcher = yield fork(getUserDataWatcher); // no need to sotre if no cancel
-  yield fork(getUserDataWatcher);
+  const watcher = yield takeLatest(LOAD_PROFILE, getUserProfileData); // no need to sotre if no cancel
+  // yield fork(getUserProfileDataWatcher);
 
   // Suspend execution until location changes
-  // yield take(LOCATION_CHANGE);
-  // yield cancel(watcher);
+  yield take(LOCATION_CHANGE);
+  yield cancel(watcher);
 }
 
 // Bootstrap sagas
 export default [
-  userData,
+  userProfileData,
 ];
